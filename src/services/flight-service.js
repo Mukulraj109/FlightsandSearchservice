e057fd70c856f56b9e3ba178f58c508dca56cbfa
Mukul Raj <104729457+Mukulraj109@users.noreply.github.com>
2024-08-17 19:27:53 +0530

    Update flight-service.js

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -1,72 +1,88 @@
-const {FlightRespository, AirplaneRespository} = require('../repository/index');
-const { compareTime } = require('../utils/helper');
+const {FlightRepository,AirlplaneRepository}=require("../repository/index");
+const { compareTime }=require("../utils/compareTime-helper");
+const { Op }=require("sequelize");
 
-class FlightService {
+class FlightService{
 
-    constructor() {
-        this.airplaneRespository = new AirplaneRespository();
-        this.flightrespository = new FlightRespository();
+    //This function is the Business Logic which must be the part of service
+    #createFilter(data){
+        let filter={};
+
+        let comb=[];
+        if(data.minPrice){
+            comb.push({price:{[Op.gte]:data.minPrice}});
+        }
+
+        if(data.maxPrice){
+            comb.push({price:{[Op.lte]:data.maxPrice}});
+        }
+
+        Object.assign(filter,{[Op.and]:comb});
+
+        return filter;
+
     }
 
-    async createFlight(data) {
+    constructor(){
+        this.flightRepository= new FlightRepository();
+        this.airplaneRepository=new AirlplaneRepository();
+    }
+
+    async createFlight(data){
         try {
-            if(!compareTime(data.arrivalTime, data.departureTime)) {
-                throw {error: 'Arrival time cannot be less than departure time'};
+            
+            if(!compareTime(data.departureTime,data.arrivalTime)){
+                throw {error:"Departure Time cannot be greater than Arrival Time"};
             }
-            const airplane = await this.airplaneRespository.getAirplane(data.airplaneId);
-            const flight = await this.flightrespository.createFlight({
-                ...data, totalSeats:airplane.capacity 
-            });
+
+            const airplane= await this.airplaneRepository.getAirplane(data.airplaneId);
+            const capacity=airplane.capacity;
+
+            const flight= await this.flightRepository.createFlight({
+                ...data, totalSeats:capacity
+             });
             return flight;
+
         } catch (error) {
-            console.log("Something went wrong at service layer");
+            console.log("Error in Service Layer");
             throw {error};
         }
     }
 
-    async getAllFlightData(data) {
+    async getFlight(id){
         try {
-            const flights = await this.flightrespository.getAllFlights(data);
+            
+            const flight = this.flightRepository.getFlight(id);
+            return flight;
+            
+        } catch (error) {
+            console.log("Error in Service Layer");
+            throw {error};
+        }
+    }
+
+    async getAllFlight(data){
+        try {
+
+            const filter= this.#createFilter(data);
+            const flights=await this.flightRepository.getAllFlight(filter);
             return flights;
+            
         } catch (error) {
-            console.log("Something went wrong at service layer");
+            console.log("Error in Service Layer");
             throw {error};
         }
     }
 
-    async getFlight(flightId) {
+    async updateFlight(flightId, data){
         try {
-            const flight = await this.flightrespository.getFlight(flightId);
+            const flight = await this.flightRepository.updateFlight(flightId, data);
             return flight;
         } catch (error) {
-            console.log("Something went wrong at service layer");
+            console.log("Error in Service Layer");
             throw {error};
         }
     }
-
-    async updateFlight(flightId, data) {
-        try {
-            const response = await this.flightrespository.updateFlights(flightId, data);
-            return response;
-        } catch (error) {
-            console.log("Something went wrong at service layer");
-            throw {error};
-        }
-    }
-    
 }
 
-module.exports = FlightService;
-
-/**
- * {
- *   flightNumber,
- *  airplaneId ,
- *   departureAirportId,
- *   arrivalAirportId,
- *   arrivalTime,
- *   departureTime,
- *   price
- *   totalSeats -> airplane
- * }
- */
+module.exports= FlightService;

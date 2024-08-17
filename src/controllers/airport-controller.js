a5351cc4332023b23200275c9548d076d9018d9d
Mukul Raj <104729457+Mukulraj109@users.noreply.github.com>
2024-08-17 19:08:44 +0530

    Update airport-controller.js

--- src/controllers/airport-controller.js
+++ src/controllers/airport-controller.js
@@ -1,27 +1,126 @@
-const { AirportService } = require('../services/index');
+const {AirportService} = require("../services/index");
 
-const airportService = new AirportService();
+const airportService= new AirportService();
 
-const create = async (req, res) => {
+const create= async(req,res)=>{
     try {
-        const response = await airportService.create(req.body);
+
+        const response= await airportService.create(req.body);
         return res.status(201).json({
-            message: 'Successfully created the airport',
-            err: {},
             data: response,
-            success: true
+            success: true,
+            message: "Airport created Successfully",
+            err:{}
         })
+        
     } catch (error) {
+        console.log("Error in Controller Layer");
         console.log(error);
         return res.status(500).json({
             data: {},
             success: false,
-            err: error,
-            message: 'Cannot create a new airport'
-        })
+            message: "Unable to create Airport",
+            err:{error}
+        });
     }
 }
 
-module.exports = {
-    create
+const update= async(req,res)=>{
+    try {
+
+        const response= await airportService.update(req.params.id,req.body);
+        return res.status(200).json({
+            data: response,
+            success: true,
+            message: "Airport Updated Successfully",
+            err:{}
+        })
+        
+    } catch (error) {
+        console.log("Error in Controller Layer");
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: "Unable to update Airport",
+            err:{error}
+        });
+    }
+}
+
+const destroy= async(req,res)=>{
+    try {
+
+        const response= await airportService.delete(req.params.id);
+        return res.status(200).json({
+            data: response,
+            success: true,
+            message: "Airport deleted Successfully",
+            err:{}
+        })
+        
+    } catch (error) {
+        console.log("Error in Controller Layer");
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: "Unable to delete Airport",
+            err:{error}
+        });
+    }
+}
+
+const get= async(req,res)=>{
+    try {
+
+        const response= await airportService.get(req.params.id);
+        return res.status(200).json({
+            data: response,
+            success: true,
+            message: "Airport fetched Successfully",
+            err:{}
+        })
+        
+    } catch (error) {
+        console.log("Error in Controller Layer");
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: "Unable to fetch Airport",
+            err:{error}
+        });
+    }
+}
+
+const getAll= async(req,res)=>{
+    try {
+
+        const response= await airportService.getAll();
+        return res.status(200).json({
+            data: response,
+            success: true,
+            message: "Airports fetched Successfully",
+            err:{}
+        })
+        
+    } catch (error) {
+        console.log("Error in Controller Layer");
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success: false,
+            message: "Unable to fetch Airports",
+            err:{error}
+        });
+    }
+}
+
+module.exports= {
+    create,
+    destroy,
+    update,
+    get,
+    getAll
 }
